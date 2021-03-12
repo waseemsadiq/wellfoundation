da44c9668fb2edfe6607626ce69d39fe7fb3376e
waseemsadiq <waseem2202@gmail.com>
2021-03-12 04:25:05 +0000

    Doctor name placeholder is in the wrong place

--- partials/forms/sports.html.php
+++ partials/forms/sports.html.php
@@ -104,7 +104,7 @@
       <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="DoctorsName">Doctor's Name</label>
     </div>
     <div class="md:w-2/3">
-      <input class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="DoctorsName" name="DoctorsName" type="text">
+      <input class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="DoctorsName" name="DoctorsName" type="text" placeholder="Dr Khan">
     </div>
   </div>
 
@@ -113,7 +113,7 @@
       <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="DoctorsNumber">Doctor's Number</label>
     </div>
     <div class="md:w-2/3">
-      <input class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="DoctorsNumber" name="DoctorsNumber" type="text" placeholder="Dr Khan">
+      <input class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="DoctorsNumber" name="DoctorsNumber" type="text">
     </div>
   </div>
 
