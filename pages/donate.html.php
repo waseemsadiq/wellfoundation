5e8ce9c886a379a58114114d08535782f6224707
waseemsadiq <waseem2202@gmail.com>
2021-03-12 15:13:26 +0000

    Update phone icon on SMS donate box

--- pages/donate.html.php
+++ pages/donate.html.php
@@ -102,7 +102,7 @@ visible: false
               <div class="lg:w-1/2 px-6 mt-4 lg:mt-0">
                 <div class="bg-blue-100 border-t-4 border-blue-500 rounded-b text-blue-900 px-4 py-3 shadow-md" role="alert">
                   <div class="flex">
-                    <div class="py-1"><svg class="fill-current h-6 w-6 text-blue-500 mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg></div>
+                    <div class="py-1"><svg class="stroke-current h-6 w-6 text-blue-500 mr-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z" /></svg></div>
                     <div>
                       <p class="font-bold">Donation by SMS</p>
                       <p class="leading-relaxed">You can also make a donation by SMS (short message service) by texting Well78 £5 to 70070 You will be billed £5 (five pounds sterling) by your mobile phone operator.</p>
