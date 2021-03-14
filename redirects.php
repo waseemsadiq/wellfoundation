735182b64b76d7648f705faba13e86e402dcec57
waseemsadiq <waseem2202@gmail.com>
2021-03-14 17:13:52 +0000

    Add redirects for old crawled links

--- redirects.php
+++ redirects.php
@@ -1,5 +1,23 @@
 <?php
 
 return array(
-    //'/justgiving/wfwells' => 'https://justgiving.com/wfwells',
+    '/Eye-camp' 						=> '/health-and-livelihoods',
+    '/eye-camp' 						=> '/health-and-livelihoods',
+    '/livelihoods' 						=> '/health-and-livelihoods/#livelihoods',
+    '/monthly-blessings' 				=> '/26th-initiative-sadaqah-project',
+    '/well-foundation-sports-academy' 	=> '/sports-academy',
+    '/component/tags/tag/events' 		=> '/sports-academy',
+    '/projects/sports-academy' 			=> '/sports-academy',
+    '/about' 							=> '/about-well-foundation',
+    '/waterwellproject' 				=> '/water',
+    '/shop'								=> '/forms/hoodies',
+    '/shop/sports-hoodie' 				=> '/forms/hoodies',
+    '/well-foundation-hoodies' 			=> '/forms/hoodies',
+    '/announcements' 					=> 'https://www.facebook.com/wellfoundation',
+    '/qurbani'							=> '/forms/qurbani',
+    '/bepartners'						=> '/business-partners',
+    '/Humanitarian-Cup'					=> '/forms/humanitarian-cup',
+    '/well-foundation-7s-league'		=> '/forms/well-foundation-7s-league',
+    '/7s-league'						=> '/forms/7s-league',
+    '/projects'							=> '/',
 );
