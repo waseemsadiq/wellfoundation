371bdd779b1162bd5c89f4ccc74e0150bd00c848
waseemsadiq <waseem2202@gmail.com>
2021-03-14 04:39:38 +0000

    #24 Add hover effect for project blobs

--- pages/index.html.php
+++ pages/index.html.php
@@ -25,7 +25,7 @@ visible: false
                     
                     <!-- BEGIN FT1 -->
                     <a href="/water" class="relative flex flex-col items-center justify-center w-full h-full p-20 mr-5 rounded-lg">
-                        <svg class="absolute z-0 w-full h-full text-blue-50 fill-current" viewBox="0 0 377 340" xmlns="http://www.w3.org/2000/svg">
+                        <svg class="absolute z-0 w-full h-full text-blue-50 hover:text-blue-100 transition-all duration-200 ease fill-current" viewBox="0 0 377 340" xmlns="http://www.w3.org/2000/svg">
                             <path d="M342.8 3.7c24.7 14 18.1 75 22.1 124s18.6 85.8 8.7 114.2c-9.9 28.4-44.4 48.3-76.4 62.4-32 14.1-61.6 22.4-95.9 28.9-34.3 6.5-73.3 11.1-95.5-6.2-22.2-17.2-27.6-56.5-47.2-96C38.9 191.4 5 151.5.9 108.2-3.1 64.8 22.7 18 61.8 8.7c39.2-9.2 91.7 19 146 16.6 54.2-2.4 110.3-35.6 135-21.6z" />
                         </svg>
                         <svg class="relative w-20 h-20" viewBox="0 0 58 58" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
@@ -73,7 +73,7 @@ visible: false
 
                     <!-- BEGIN FT2 -->
                     <a href="/food" class="relative flex flex-col items-center justify-center w-full h-full p-20 mr-5 rounded-lg">
-                        <svg class="absolute z-0 w-full h-full text-blue-50 fill-current" viewBox="0 0 358 372" xmlns="http://www.w3.org/2000/svg">
+                        <svg class="absolute z-0 w-full h-full text-blue-50 hover:text-blue-100 transition-all duration-200 ease fill-current" viewBox="0 0 358 372" xmlns="http://www.w3.org/2000/svg">
                             <path d="M315.7 6.5c30.2 15.1 42.6 61.8 41.5 102.5-1.1 40.6-15.7 75.2-24.3 114.8-8.7 39.7-11.3 84.3-34.3 107.2-23 22.9-66.3 23.9-114.5 30.7-48.2 6.7-101.3 19.1-123.2-4.1-21.8-23.2-12.5-82.1-21.6-130.2C30.2 179.3 2.6 141.9.7 102c-2-39.9 21.7-82.2 57.4-95.6 35.7-13.5 83.3 2.1 131.2 1.7 47.9-.4 96.1-16.8 126.4-1.6z" />
                         </svg>
                         <svg class="relative w-20 h-20" viewBox="0 0 58 58" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
@@ -120,7 +120,7 @@ visible: false
 
                     <!-- BEGIN FT2 -->
                     <a href="/education" class="relative flex flex-col items-center justify-center w-full h-full p-20 mr-5 rounded-lg">
-                        <svg class="absolute z-0 w-full h-full text-blue-50 fill-current" viewBox="0 0 378 410" xmlns="http://www.w3.org/2000/svg">
+                        <svg class="absolute z-0 w-full h-full text-blue-50 hover:text-blue-100 transition-all duration-200 ease fill-current" viewBox="0 0 378 410" xmlns="http://www.w3.org/2000/svg">
                             <path d="M305.9 14.4c23.8 24.6 16.3 84.9 26.6 135.1 10.4 50.2 38.6 90.3 43.7 137.8 5.1 47.5-12.8 102.4-50.7 117.4-37.9 15.1-95.7-9.8-151.7-12.2-56.1-2.5-110.3 17.6-130-3.4-19.7-20.9-4.7-82.9-11.5-131.2C25.5 209.5-3 174.7 1.2 147c4.2-27.7 41-48.3 75-69.6C110.1 56.1 141 34.1 184 17.5c43.1-16.6 98.1-27.7 121.9-3.1z" />
                         </svg>
 
@@ -163,7 +163,7 @@ visible: false
 
                     <!-- BEGIN FT3 -->
                     <a href="/health-and-livelihoods" class="relative flex flex-col items-center justify-center w-full h-full p-20 mr-5 rounded-lg">
-                        <svg class="absolute z-0 w-full h-full text-blue-50 fill-current" viewBox="0 0 440 440" xmlns="http://www.w3.org/2000/svg">
+                        <svg class="absolute z-0 w-full h-full text-blue-50 hover:text-blue-100 transition-all duration-200 ease fill-current" viewBox="0 0 440 440" xmlns="http://www.w3.org/2000/svg">
                             <path d="M442,314Q449,378,395,413.5Q341,449,279,466Q217,483,155.5,456.5Q94,430,99,361.5Q104,293,70,240Q36,187,75,139.5Q114,92,165.5,57Q217,22,276.5,42Q336,62,364,110.5Q392,159,413.5,204.5Q435,250,442,314Z"></path>
                         </svg>
 
@@ -207,7 +207,7 @@ visible: false
                     <!-- BEGIN FT4 -->
 
                     <a href="/mosques" class="relative flex flex-col items-center justify-center w-full h-full p-20 mr-5 rounded-lg">
-                        <svg class="absolute z-0 w-full h-full text-blue-50 fill-current" viewBox="0 0 440 440" xmlns="http://www.w3.org/2000/svg">
+                        <svg class="absolute z-0 w-full h-full text-blue-50 hover:text-blue-100 transition-all duration-200 ease fill-current" viewBox="0 0 440 440" xmlns="http://www.w3.org/2000/svg">
                             <path d="M413,288Q407,326,385,358.5Q363,391,329.5,421Q296,451,247.5,463Q199,475,150,455.5Q101,436,92,383Q83,330,45.5,290Q8,250,21,198Q34,146,92.5,136Q151,126,173,68Q195,10,246.5,25.5Q298,41,346.5,54.5Q395,68,410.5,116.5Q426,165,422.5,207.5Q419,250,413,288Z"></path>
                         </svg>
 
@@ -250,7 +250,7 @@ visible: false
 
                     <!-- BEGIN FT5 -->
                     <a href="/26th-initiative-sadaqah-project" class="relative flex flex-col items-center justify-center w-full h-full p-20 mr-5 rounded-lg">
-                        <svg class="absolute z-0 w-full h-full text-blue-50 fill-current" viewBox="0 0 440 440" xmlns="http://www.w3.org/2000/svg">
+                        <svg class="absolute z-0 w-full h-full text-blue-50 hover:text-blue-100 transition-all duration-200 ease fill-current" viewBox="0 0 440 440" xmlns="http://www.w3.org/2000/svg">
                             <path d="M439.5,299Q454,348,430,396.5Q406,445,348,434.5Q290,424,244.5,449Q199,474,166,435.5Q133,397,96.5,369.5Q60,342,71,296Q82,250,59.5,198.5Q37,147,84,123.5Q131,100,171,90.5Q211,81,249,84Q287,87,329.5,92Q372,97,390.5,135.5Q409,174,417,212Q425,250,439.5,299Z"></path>
                         </svg>
 
@@ -298,7 +298,7 @@ visible: false
 
                     <!-- BEGIN FT6 -->
                     <a href="/sports-academy" class="relative flex flex-col items-center justify-center w-full h-full p-20 mr-5 rounded-lg">
-                        <svg class="absolute z-0 w-full h-full text-blue-50 fill-current" viewBox="0 0 358 372" xmlns="http://www.w3.org/2000/svg">
+                        <svg class="absolute z-0 w-full h-full text-blue-50 hover:text-blue-100 transition-all duration-200 ease fill-current" viewBox="0 0 358 372" xmlns="http://www.w3.org/2000/svg">
                             <path d="M315.7 6.5c30.2 15.1 42.6 61.8 41.5 102.5-1.1 40.6-15.7 75.2-24.3 114.8-8.7 39.7-11.3 84.3-34.3 107.2-23 22.9-66.3 23.9-114.5 30.7-48.2 6.7-101.3 19.1-123.2-4.1-21.8-23.2-12.5-82.1-21.6-130.2C30.2 179.3 2.6 141.9.7 102c-2-39.9 21.7-82.2 57.4-95.6 35.7-13.5 83.3 2.1 131.2 1.7 47.9-.4 96.1-16.8 126.4-1.6z" />
                         </svg>
                         <svg class="relative w-20 h-20" viewBox="0 0 58 58" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
