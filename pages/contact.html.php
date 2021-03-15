b51d7c9f8a3144f451254d00093a08360377459d
waseemsadiq <waseem2202@gmail.com>
2021-03-15 00:48:57 +0000

    Add our accessibility contrast for for send buttons back in
    We fixed the paragraph margin that was suspected to be the bug

--- pages/contact.html.php
+++ pages/contact.html.php
@@ -61,7 +61,7 @@ form:
               <div class="md:flex md:items-center">
                 <div class="md:w-1/3"><?= helper('form.honeypot', page()->form->honeypot); ?></div>
                 <div class="md:w-2/3">
-                  <button class="shadow bg-purple-500 hover:bg-purple-400 focus:ring focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
+                  <button class="shadow bg-purple-600 hover:bg-purple-500 focus:ring focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
                     Send
                   </button>
                 </div>
