b31cd94e01003dbd98be29a10e199a8cb06b0dc0
waseemsadiq <waseem2202@gmail.com>
2021-03-15 02:13:09 +0000

    #33 Add active menu style for the mainmenu partial

--- partials/navigation/mainmenu.html.php
+++ partials/navigation/mainmenu.html.php
@@ -1,4 +1,22 @@
 <? $pages = collection('pages', ['folder' => $folder ?? '.', 'level' => 3,  'recurse' => 'true', 'filter' => ['visible' => 'true']])  ?>
 <? foreach ($pages as $page) :?>
-<a  href="<?= route($page); ?>" class="block md:inline-block text-base   px-4 py-2 leading-none rounded border-white hover:border-transparent hover:text-indigo-700 hover:bg-white mt-4 md:mt-0"><?= $page->name; ?></a>
+<a 
+	href="<?= route($page); ?>" 
+	class="
+	block 
+	md:inline-block 
+	text-base 
+	px-4 
+	py-2 
+	leading-none 
+	rounded 
+	border-white 
+	hover:border-transparent 
+	hover:text-indigo-700 
+	hover:bg-white 
+	mt-4 
+	md:mt-0
+	<?= strpos(page()->path, $page->path) === 0 ? ' text-indigo-700 hover:text-gray-500 is-active' : '' ?>">
+	<?= $page->name; ?>
+</a>
 <? endforeach ?>
