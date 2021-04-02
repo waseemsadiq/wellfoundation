e661d99c725d5beeab5b15160c254f71f8d4362a
waseemsadiq <waseem2202@gmail.com>
2021-04-03 00:56:22 +0100

    Add some content for context for the food programme countries

--- partials/images/images-data-grid.html.php
+++ partials/images/images-data-grid.html.php
@@ -6,9 +6,10 @@
 
 <div class="grid grid-cols-2 gap-16 mb-16 text-center lg:grid-cols-6">
 	<? foreach(data($data)->shuffle() as $item) : ?>
-		<div class="flex flex-col items-center justify-center">
-			<img class="block object-contain h-32 tns-lazy" src="images://<?= $data ?>/<?= $item->image ?>" alt="Map of <?= $item->name ?>" data-lazyload="progressive,inline">
-			<p class="block mt-2"><?= $item->name; //var_dump($item->image); ?></p>
+		<div class="flex flex-col content-start text-center">
+			<img class="object-contain h-32 tns-lazy" src="images://<?= $data ?>/<?= $item->image ?>" alt="Map of <?= $item->name ?>" data-lazyload="progressive,inline">
+			<h2 class="my-6 text-lg font-black leading-tight text-gray-900 sm:text-2xl"><?= $item->name; ?></h2>
+			<p class="text-gray-500"><?= $item->description; ?></p>
 		</div>
 	<? endforeach; ?>
 </div>
