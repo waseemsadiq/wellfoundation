55f59943d97c953e082d845f14aa62c19dd64340
waseemsadiq <waseem2202@gmail.com>
2021-04-07 00:13:18 +0100

    #1150 Add working JustGiving model
    conributed by Johan Janssens (http://about.me/johanjanssens)

--- extensions/pages/model/justgiving.php
+++ extensions/pages/model/justgiving.php
@@ -1,26 +1,13 @@
 <?php
-class ExtPagesModelJustGiving extends ComPagesModelWebservice
+class ExtPagesModelJustgiving extends ComPagesModelWebservice
 {
     protected function _initialize(KObjectConfig $config)
     {
         $config->append([
-            'data_path'   => 'fundraisingPages',
-            'cache_path'  =>  $this->getObject('com://site/pages.config')->getSitePath('cache').'/justgiving',
+            'data_path'    => 'fundraisingPages/fundraisingPage',
+            'identity_key' => 'pageId',
+            'cache_path'   => $this->getObject('com://site/pages.config')->getSitePath('cache') . '/justgiving',
         ]);
         parent::_initialize($config);
-    }
-    public function fetchData($count = false)
-    {
-        $data = parent::fetchData();
-        array_walk($data, function(&$item)
-        {
-            $item += $item['fundraisingPage'];
-            unset($item['fundraisingPage']);
-        });
-        //Check if the database has a lastModified column
-        if(!$this->_hash_key && array_key_exists('lastModified', $data[0])) {
-            $this->_hash_key = array('lastModified');
-        }
-        return $data;
     }
 }
--- pages/justgiving/index.html.php
+++ pages/justgiving/index.html.php
@@ -1,30 +1,43 @@
 ---
 route: justgiving/[digit:page]?
 layout: default
-summary: Just Giving test page 
+summary: Just Giving test page
 image: images://food-cover.jpg
 visible: false
 collection:
     model: justgiving
     config:
         url: https://api.justgiving.com/c529958f/v1/account/fahim.baqir@wellfoundation.org.uk/pages?charityid=187701
-        identity_key: slug
-
-
-#        url: https://api.justgiving.com/c529958f/v1/charity/187701
 ---
+<? 
+	//var_dump(count(collection()));return;
+	//var_dump($event); return; 
+?>
+    <div class="relative px-8 py-10 border-t border-gray-200 md:py-16 lg:py-24 xl:py-40 xl:px-0">
+      <!-- JustGiving Projects -->
+      <div id="countries" class="container flex flex-col items-center h-full max-w-7xl mx-auto">
+          <h2 class="w-full max-w-4xl px-5 px-8 mt-2 text-2xl font-black leading-tight text-center text-gray-900 sm:mt-0 sm:px-0 sm:text-6xl md:px-0 mb-8">
+          	Our JustGiving.com projects
+          </h2>
+          <div class="flex px-5 py-24">
+            <ul>
+				<? foreach(collection() as $event): ?>
+				    <li>
+						<a href="https://justgiving.com/fundraising/<?= strtolower($event->pageShortName) ?>" rel="nofollow noreferrer" target="_blank">
+							<?= $event->eventName ?>
+						</a>
+				    	<pre style="display:none;">
+							<? 
+								//var_dump(count($event))
+								//var_dump($event); return; 
+							?>
+						</pre>
+					</li>
+				<? endforeach ?>
+				</ul>
+          </div>
+      </div>
+      <!-- End JustGiving Projects -->
 
-<?// foreach(collection()->get('charity')['categories']['a:string'] as $category): ?>
-	<?// var_dump($category) ?>
-<?// endforeach ?>
+    </div>
 
-<? var_dump(collection()) ?>
-
-<a 
-	href="https://link.staging.justgiving.com/v1/charity/donate/charityId/187701?donationValue=25&currency=GBP&exiturl=http%3A%2F%2Fwww.wellfoundation.ork.uk?jgDonationId=JUSTGIVING-DONATION-ID&reference=demobutton" 
-	class="
-		button 
-		<?= (isset($button_colour)) ? 'bg-'.$button_colour.'-600' : 'bg-indigo-600' ?> 
-		<?= (isset($button_colour)) ? 'hover:bg-'.$button_colour.'-400' : 'hover:bg-indigo-400' ?>">
-	<?= (isset($button_text)) ? $button_text : 'Donate Today!' ?>
-</a>
