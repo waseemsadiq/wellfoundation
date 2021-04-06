fbb47f2f77ba03b32208f7d6a23394657915571a
waseemsadiq <waseem2202@gmail.com>
2021-04-06 21:54:46 +0100

    #1150 update index page to use the model

--- pages/justgiving/index.html.php
+++ pages/justgiving/index.html.php
@@ -1,21 +1,24 @@
 ---
+route: justgiving/[digit:page]?
 layout: default
 summary: Just Giving test page 
 image: images://food-cover.jpg
 visible: false
 collection:
-    model: webservice
+    model: justgiving
     config:
-#        url: https://api.justgiving.com/c529958f/v1/charity/187701
-
         url: https://api.justgiving.com/c529958f/v1/account/fahim.baqir@wellfoundation.org.uk/pages?charityid=187701
+        identity_key: slug
+
+
+#        url: https://api.justgiving.com/c529958f/v1/charity/187701
 ---
 
 <?// foreach(collection()->get('charity')['categories']['a:string'] as $category): ?>
 	<?// var_dump($category) ?>
 <?// endforeach ?>
 
-<? //var_dump(collection()->get('fundraisingPages')['fundraisingPage']) ?>
+<? var_dump(collection()) ?>
 
 <a 
 	href="https://link.staging.justgiving.com/v1/charity/donate/charityId/187701?donationValue=25&currency=GBP&exiturl=http%3A%2F%2Fwww.wellfoundation.ork.uk?jgDonationId=JUSTGIVING-DONATION-ID&reference=demobutton" 
